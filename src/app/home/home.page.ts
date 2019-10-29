aff47dab0a0bfe6ac558311c4290a1621f2a0eea
pablozermenoga <pzg97@hotmail.com>
2019-10-29 11:46:54 -0700

    AQUINO ZERMEÑO

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormGroup,Validators,FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  myForm : FormGroup;
+  constructor(private fb:FormBuilder) {}
+
+  ngOnInit(){
+    this.myForm=this.fb.group({
+      nombre:["", Validators.compose([Validators.required,Validators.minLength(10)])],
+      email:["",Validators.compose([Validators.required,Validators.maxLength(320),Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')])],
+      telefono:["",Validators.required],
+      rfc:["",Validators.compose([Validators.required,Validators.pattern('[A-Z,Ñ,&]{3,4}[0-9]{2}[0-1][0-9][0-3][0-9][A-Z,0-9]?[A-Z,0-9]?[0-9,A-Z]?')])],
+      curp:["",Validators.compose([Validators.required,Validators.pattern('^[A-Z]{1}[AEIOU]{1}[A-Z]{2}[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])[HM]{1}(AS|BC|BS|CC|CS|CH|CL|CM|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)[B-DF-HJ-NP-TV-Z]{3}[0-9A-Z]{1}[0-9]{1}$')])],
+      genero:["",Validators.required]
+    });
+  }
 
 }
